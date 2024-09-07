fdb6a3c0d9156b8b474deda5c12207dcf72a4670
lucas794 <lgtorres@fi.uba.ar>
2024-09-07 14:02:30 -0300

    fixed typo on errors

--- src/errors.rs
+++ src/errors.rs
@@ -11,8 +11,8 @@ impl<'a> Display for TPErrors<'a> {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
         match self {
             TPErrors::InvalidGeneric(e) => write!(f, "[ERROR] {}", *e),
-            TPErrors::InvalidTable(e) => write!(f, "[INVALID_TABLE]: Error {}", *e),
-            TPErrors::InvalidSyntax(e) => write!(f, "[INVALID_SYNTAX]: Error {}", *e),
+            TPErrors::InvalidTable(e) => write!(f, "[INVALID_TABLE]: {}", *e),
+            TPErrors::InvalidSyntax(e) => write!(f, "[INVALID_SYNTAX]: {}", *e),
         }
     }
 }
