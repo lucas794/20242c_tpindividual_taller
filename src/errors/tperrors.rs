07493058c4029bd7170725a4412fac3fd1c03e61
lucas794 <lgtorres@fi.uba.ar>
2024-09-21 11:57:26 -0300

    fixed minimal typo on errors

--- src/errors/tperrors.rs
+++ src/errors/tperrors.rs
@@ -10,7 +10,7 @@ pub enum Tperrors {
 impl Display for Tperrors {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
         match self {
-            Tperrors::Generic(e) => write!(f, "ERROR {}", e),
+            Tperrors::Generic(e) => write!(f, "ERROR: {}", e),
             Tperrors::Table(e) => write!(f, "INVALID_TABLE: {}", e),
             Tperrors::Syntax(e) => write!(f, "INVALID_SYNTAX: {}", e),
         }
