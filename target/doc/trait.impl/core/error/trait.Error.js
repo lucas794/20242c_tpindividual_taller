23f9e6eb8b39221a03c668c171fc9335cf2282ae
lucas794 <lgtorres@fi.uba.ar>
2024-09-24 08:15:32 -0300

    Documentation changed

--- target/doc/trait.impl/core/error/trait.Error.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"tp_individual":[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.80.1/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"tp_individual/errors/enum.TPErrors.html\" title=\"enum tp_individual::errors::TPErrors\">TPErrors</a>&lt;'a&gt;"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
